36b684d84311694af9b0681cd819d33dcb47e5ae
Yash Tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-11-15 16:42:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ mongoose.connect("mongodb://localhost:27017/test-internhsip");
 const app = express();
 
 const regSchema = mongoose.Schema({
-  reg: String,
+  reg : String,
 });
 
 const RegModel = mongoose.model("reg247", regSchema, "reg247"); 
@@ -25,4 +25,4 @@ app.get('/all', async (req, res) => {
 
 app.listen(8001, () => {
   console.log("Server started at port localhost:8001");
-});
+})
