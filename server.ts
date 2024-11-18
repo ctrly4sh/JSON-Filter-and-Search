3c41a81366da50423eb77da50407d6a1d2152de7
Yash Tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-11-18 15:47:03 +0530

    project setup

---
+++ server.ts
@@ -0,0 +1,20 @@
+import express , {Request, Response} from "express";
+import router from "./routes/authRoute"
+
+const app = express();
+
+const PORT : number = 3000;
+
+app.use(express.json());
+
+app.get('/' , (req : Request , res : Response) => {
+    res.send("Typescript + node")
+});
+
+app.get('/all' , router)
+app.post('/register' , router)
+app.post("/login" , router)
+
+app.listen(PORT , () => {
+    console.log(`Server started at localhost:${PORT}`)
+})
